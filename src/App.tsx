d12cf6e5fa6f75d504d1dc5538e9f888d6128ed6
Yvan-David <irankundayvan2020@gmail.com>
2024-06-11 23:08:02 +0200

    chore(setup): set sass and tailwind in vite - setup sass in vite - setup tailwind css in vite - test sass and tailwind
    [Delivers #187364672]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Counter from "./components/counter"
+import './app.scss'
 function App() {
   return (
     <>
     <Counter/>
+    <div className="nav-bar">Bit.Shop(sass)
+      <div className="sub-nav">Home (sass)</div>
+    </div>
+    <div className="font-bold text-xl text-green-500">About (Tailwind)</div>
     </>
+
   )
 }
 
