62599fb4af1c0ffe051c59e9f429f95987123edb
Mpano Athos <84918863+mpanoathos@users.noreply.github.com>
2024-06-14 09:14:57 +0200

    Configure ESLint and Git Precommit hook to run tests (#8)
    ch(eslintconfiguration):Eslint configuration and github pre-commit
    [Delivers #187364675]

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+export default [
+  {
+    ignores: [
+      "dist",
+      "coverage",
+      "node_modules",
+      ".eslintrc.js",
+      "eslint.config.js",
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      prettier: require("eslint-plugin-prettier"),
+      jest: require("eslint-plugin-jest"),
+      "testing-library": require("eslint-plugin-testing-library"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+    },
+  },
+];
